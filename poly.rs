18f06e28d3110cda9280d58292d3900acf98a3d6
Johnny <147927627+acetij@users.noreply.github.com>
2026-06-25 16:30:27 -0400

    Update poly.rs
    Refined the executable functions with Verus code and added proofs. Polynomial addition being associative still needs to be verified.

--- poly.rs
+++ poly.rs
@@ -1,6 +1,5 @@
 /*poly.rs*/
 use vstd::prelude::*;
-//use crate::arith::*;
 use crate::arith::add_mod_Q;
 use crate::arith::sub_mod_Q;
 use crate::arith::Q;
@@ -15,33 +14,30 @@ pub struct Poly {
 }
 
 /* SPEC FUNCTIONS*/
-/*
-pub open spec fn is_valid_coeff(p: &Poly) -> bool {
-    forall |i: int| 0 <= i < N as int ==> p.coeffs[i as int] < Q as u16
-}
-*/
 
+//are polynomial coeffs vaid?
 pub open spec fn is_poly_valid(p: &Poly) -> bool {
     forall |i: int| 0 <= i < N as int ==> p.coeffs[i] < Q as u16
 }
-
-/*
-pub open spec fn spec_poly_add(p: &Poly, q: &Poly) -> Poly {
-    Poly {
-        coeffs: Seq::new(N as nat, |i: nat|
-                    (p.coeffs[i as usize] + b.coeffs[i as usize]) % Q as u16
-        )
-    }
-}
-*/
 
 //Two polynomials are equivalent
 pub open spec fn poly_eq(p: &Poly, q: &Poly) -> bool {
     forall |i: int| 0 <= i < N as int ==> p.coeffs[i] == q.coeffs[i]
 }
 
+//zero polynomial
 pub open spec fn spec_poly_zero() -> Poly {
     Poly {coeffs: [0u16; N]}
+}
+
+//
+pub open spec fn spec_poly_add_coeff(p: &Poly, q: &Poly, i: int) -> u16 {
+    ((p.coeffs[i] as int + q.coeffs[i] as int) % Q as int) as u16
+}
+
+pub open spec fn poly_add_eq(p: &Poly, q: &Poly, result: &Poly) -> bool {
+    forall |i: int| 0 <= i < N as int ==> 
+        result.coeffs[i] == spec_poly_add_coeff(p,q,i)
 }
 
 
@@ -87,16 +83,75 @@ pub fn poly_add_Q(r: &Poly, s: &Poly) -> (result: Poly)
     result
 }
 
-
-
-/*
-pub fn poly_sub_Q(r: &Poly, s: &Poly) -> Poly {
+pub fn poly_sub_Q(r: &Poly, s: &Poly) -> (result: Poly)
+    requires
+        is_poly_valid(r),
+        is_poly_valid(s),
+    ensures
+        is_poly_valid(&result)
+{
     let mut result = poly_zero();
-    for i in 0..N {
+    let mut i = 0usize;
+    while i < N
+        invariant
+            i <= N,
+            is_poly_valid(r),
+            is_poly_valid(s),
+            forall |j: int| 0 <= j < i ==> result.coeffs[j] < Q as u16,
+        decreases
+            N - i
+    {
+        assert(is_valid_coeff(r.coeffs[i as int])) by {
+            assert(is_poly_valid(r));
+        }
+        assert(is_valid_coeff(s.coeffs[i as int])) by {
+            assert(is_poly_valid(s));
+        }
+
         result.coeffs[i] = sub_mod_Q(r.coeffs[i], s.coeffs[i]);
+        i += 1
     }
     result
 }
+
+
+/*PROOFS*/
+
+//The Zero polynomial is valid
+proof fn lemma_poly_zero_valid()
+    ensures is_poly_valid(&spec_poly_zero()),
+{}
+
+//commutativity of polynomial addition
+proof fn lemma_poly_add_commutative(p: &Poly, q: &Poly)
+    requires
+        is_poly_valid(p),
+        is_poly_valid(q),
+    ensures
+        forall |i: int| 0 <= i < N as int ==>
+            spec_poly_add_coeff(p,q,i) == spec_poly_add_coeff(q,p,i)
+
+{}
+
+//addative identity of polynomials is obeyed 
+proof fn lemma_poly_add_identity(p: &Poly)
+    requires
+        is_poly_valid(p),
+    ensures
+        forall |i: int| 0 <= i < N as int ==>
+            spec_poly_add_coeff(p, &spec_poly_zero(), i) == p.coeffs[i],
+{}
+
+/*
+//associatvitiy of polynomial addition
+proof fn lemma_poly_assoc(p: &Poly, q: &Poly, r:&Poly)
+    requires
+        is_poly_valid(p),
+        is_poly_valid(q),
+        is_poly_valid(r),
+    ensures
+       //poly_eq(&poly_add_Q(&poly_add_Q(p,q),r), &poly_add_Q(p, &poly_add_Q(q,r))),
+{}
 */
 
 #[cfg(test)]
